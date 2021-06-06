1cd2865a2ae806a4aa5e89e7d140ea929ad6e65a
presmelito <prvillanuevajr@gmail.com>
2021-06-06 22:10:43 +0800

    Minor fix

--- src/app.js
+++ src/app.js
@@ -9,8 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
+
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData)
+const users = JSON.parse(userData);
 
 app.get('/', async (req, res) => {
     res.render('index', {title: 'Account Summary', accounts})
