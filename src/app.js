ee2074507a679db1237bf81bfc8e2547b09c5a5c
presmelito <prvillanuevajr@gmail.com>
2021-06-06 22:34:39 +0800

    Phase 4

--- src/app.js
+++ src/app.js
@@ -2,17 +2,14 @@ const express = require('express')
 const app = express();
 const fs = require('fs')
 const path = require('path')
+const {writeJSON,accounts,users} = require('./data')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded())
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
 
 app.get('/', async (req, res) => {
     res.render('index', {title: 'Account Summary', accounts})
@@ -43,8 +40,7 @@ app.post('/transfer', async (req, res) => {
     accounts[req.body.from].balance -= parseInt(req.body.amount);
     accounts[req.body.to].balance += parseInt(req.body.amount);
 
-    const accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    writeJSON()
 
     res.render('transfer', {message: "Transfer Completed"})
 })
@@ -57,8 +53,7 @@ app.post('/payment', async (req, res) => {
     accounts.credit.balance -= parseInt(req.body.amount);
     accounts.credit.available += parseInt(req.body.amount);
 
-    const accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    writeJSON()
 
     res.render('payment', {message: "Payment Successful", account: accounts.credit})
 })
--- src/data.js
+++ src/data.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 
-const accountDataPath = path.join(__dirname, 'json/accounts.json');
-const accounts = JSON.parse(fs.readFileSync(accountDataPath, 'utf8'))
-const users = JSON.parse(fs.readFileSync(path.join(__dirname, 'json/users.json'), 'utf8'))
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
-const writeJson = () => {
-    fs.writeFileSync(accountDataPath, JSON.stringify(accounts));
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
+const writeJSON = () => {
+    const accountsJSON = JSON.stringify(accounts)
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
 }
 
 module.exports = {
     accounts,
     users,
-    writeJson
+    writeJSON
 }
