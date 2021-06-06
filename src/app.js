a6a36c9cd9e5fcd1cbe90c984ebadb430cb064da
presmelito <prvillanuevajr@gmail.com>
2021-06-06 22:07:32 +0800

    Fix phase 2

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
-const accounts = JSON.parse(accountData)
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData)
 
 app.get('/', async (req, res) => {
@@ -17,15 +17,15 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/savings', async (req, res) => {
-    res.render('summary', {account: accounts.savings})
+    res.render('account', {account: accounts.savings})
 })
 
 app.get('/credit', async (req, res) => {
-    res.render('summary', {account: accounts.credit})
+    res.render('account', {account: accounts.credit})
 })
 
 app.get('/checking', async (req, res) => {
-    res.render('summary', {account: accounts.checking})
+    res.render('account', {account: accounts.checking})
 })
 
 app.get('/profile', async (req, res) => {
